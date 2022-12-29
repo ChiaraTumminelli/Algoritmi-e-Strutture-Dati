6cfdb640f6c03567d91e759812cdb27b785a8dc3
Chiara Tumminelli <chiara.tumminelli@edu.unito.it>
2022-12-29 03:12:06 +0100

    Delete Exercise_1 directory

--- Exercise_1/src/comparator.h
+++
@@ -1,8 +0,0 @@
-// Comparator for int values
-int comparator_int(void *i1, void *i2);
-
-// Comparator for char values
-int comparator_char(void *c1, void *c2);
-
-// Comparator for float values
-int comparator_float(void *f1, void *f2);
